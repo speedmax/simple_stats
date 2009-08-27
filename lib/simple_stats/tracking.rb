3ba0683d54add9e66f76952c9fcaba316e8bdf84
speedmax <subjective@gmail.com>
2009-08-27 14:47:16 +1000

    refactor Tracking::conditions_for

--- lib/simple_stats/tracking.rb
+++ lib/simple_stats/tracking.rb
@@ -3,17 +3,20 @@ module SimpleStats
 
     # Build condition for search
     def conditions_for(action, from = nil, options = {})
-     from = Time.now unless from
 
-     if from.is_a? Range
-       from, to = from.first, from.last
-     else
-       to = from
-     end
-     conditions = {
-       :startkey => [action, self.id, Time.parse(from.beginning_of_day.to_s)],
-       :endkey => [action, self.id, Time.parse(to.end_of_day.to_s)]
-     }.merge(options)
+      case from
+      when Range
+        from, to = from.first, from.last
+      when Date, Time
+        to = from
+      else
+        to = from = Time.now
+      end
+
+      conditions = {
+        :startkey => [action, self.id, Time.parse(from.beginning_of_day.to_s)],
+        :endkey => [action, self.id, Time.parse(to.end_of_day.to_s)]
+      }.merge(options)
     end
   end
 end
